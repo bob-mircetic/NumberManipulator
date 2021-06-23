51d7aabbdde65549cd8243155bbcf936037c9fcb
Mircetic <17263@nelnet.net>
2021-06-23 09:17:25 -0600

    done

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace numbermanipulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            input = input * input;
+
+            input += 14;
+
+            input -= 3;
+
+            Console.WriteLine(input);
+            
+        }
+    }
+}
